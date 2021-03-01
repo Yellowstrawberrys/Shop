fd22024e48a7cdee4b0b1e7fa652c66e02df964f
CommandBlock070 <79506343+CommandBlock070@users.noreply.github.com>
2021-03-01 21:58:53 +0900

    Add files via upload

---
+++ src/main/java/shop/shop/BlockShopGUI.java
@@ -0,0 +1,65 @@
+package shop.shop;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class BlockShopGUI extends JavaPlugin {
+
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd ,String Label,String[] args) {
+        Player player = (Player) sender;
+        if (cmd.getName().equals("블럭상점")) {
+            if(sender instanceof Player) {
+
+                Inventory gui = Bukkit.createInventory(player, 54, ChatColor.BLACK + "블럭상점");
+
+
+                ItemStack close = new ItemStack(Material.BARRIER);
+                ItemMeta close_meta = close.getItemMeta();
+                close_meta.setDisplayName(ChatColor.RED + "닫기");
+
+                ItemStack block = new ItemStack(Material.GRASS_BLOCK);
+
+                gui.setItem(53, close);
+            } else {
+                log.severe("이 명령어를 버킷에서 사용하실수 없습니다");
+            }
+        }
+        return false;
+    }
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+        Player player = (Player) e.getWhoClicked();
+        if(e.getClickedInventory().equals("블럭상점")) {
+            switch (e.getCurrentItem().getType()) {
+                case BARRIER:
+                    player.closeInventory();
+                    break;
+            }
+            e.setCancelled(true);
+        }
+    }
+
+
+}
+
---
+++ src/main/java/shop/shop/Shop.java
@@ -0,0 +1,79 @@
+package shop.shop;
+
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public final class Shop extends JavaPlugin {
+
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    @Override
+    public void onEnable() {
+        getCommand("음식상점");
+        getCommand("블럭상점");
+        getCommand("인첸트상점");
+        getCommand("기타상점");
+        getCommand("광물상점");
+        getCommand("농작물상점");
+        getCommand("상점테스트");
+        if (!setupEconomy()) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+            // Plugin startup logic
+
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender,Command cmd,String Label,String[] args) {
+        Player player = (Player) sender;
+        if (cmd.getName().equals("상점테스트")) {
+            if (sender instanceof Player) {
+                player.sendMessage("테스트");
+            } else {
+                log.severe("테스트");
+            }
+        }
+        return false;
+    }
+}
